6efbbaa8c59467ab50ec69b612a8445c48bb6d35
Emad Abdulrahim <emad90mohamad@gmail.com>
2020-08-21 19:17:36 -0700

    Initial Commit

---
+++ Gallery/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Gallery
+//
+//  Created by Emad Abdulrahim on 8/21/20.
+//  Copyright © 2020 Emad Abdulrahim. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
